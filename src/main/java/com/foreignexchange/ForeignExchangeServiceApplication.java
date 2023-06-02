312ecae8a0525535bc8b827d2110dfa0c6d59f66
Selcuk Egeli <selcukegeli.tr@gmail.com>
2023-06-03 01:17:22 +0300

    project init

---
+++ src/main/java/com/foreignexchange/ForeignExchangeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.foreignexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForeignExchangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForeignExchangeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foreignexchange/ForeignExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foreignexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForeignExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
